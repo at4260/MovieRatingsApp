d04fe16243b3cc4354a8d9c472a1648b4db6f38c
Hackbright Student <info@hackbrightacademy.com>
2015-02-12 13:09:40 -0800

    accounts can be created, base.html added, flash works

--- judgement.py
+++ judgement.py
@@ -1,36 +1,53 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, flash
+from flask import session as flask_session
+from model import session as model_session
 import model
 # import jinja2
 
 app = Flask(__name__)
-app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
+app.secret_key = 'thisisasecretkey'
+# app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route("/")
 def home_page():
     return render_template("home.html")
 
 @app.route("/create")
-def create_login():
+def create_acct():
     return render_template("create.html")
+
+@app.route("/create", methods=["POST"])
+def process_acct():
+    """ToDo: use POST method to save data to database and then redirect to /login, flash that account was created succesfully"""
+    email = request.form["email"]
+    password =request.form["password"]
+    age = request.form["age"]
+    zipcode = request.form["zipcode"]
+    my_new_acct = model.User(email=email, password=password , age=age , zipcode=zipcode)
+    model_session.add(my_new_acct)
+    model_session.commit()
+    flash("Your account has been succesfully added. Please log in.")
+    return redirect("/login")
 
 @app.route("/login", methods=["GET"])
 def show_login():
     return render_template("login.html")
 
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["POST"])
 def process_login():
     """TODO: Receive the user's login credentials located in the 'request.form'
     dictionary, look up the user, and store them in the session."""
 
     email = request.form['email']
     password = request.form['password']
-    session["email"] = email   
+    flask_session["email"] = email   
     return redirect("/all_users")
 
 @app.route("/all_users")
 def index():
-    user_list = model.session.query(model.User).limit(5).all()
+    user_list = model_session.query(model.User).limit(5).all()
     return render_template("user_list.html", users=user_list)
+    """ToDo: create links for each user and send it as a GET request like the Ubermelon App"""
 
 if __name__ == "__main__":
     app.run(debug = True)
